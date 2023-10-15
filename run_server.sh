f3cf5f814620752c45e34531e968f81f750234ad
luckielordie <luckielordie@gmail.com>
2023-10-15 17:21:56 +0100

    refactor

--- run_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-INSTDIR="/home/steam/project_zomboid"
-echo "64-bit java detected"
-        export PATH="${INSTDIR}/jre64/bin:$PATH"
-        export LD_LIBRARY_PATH="${INSTDIR}/linux64:${INSTDIR}/natives:${INSTDIR}:${INSTDIR}/jre64/lib/amd64:${LD_LIBRARY_PATH}"
-        JSIG="libjsig.so"
-        LD_PRELOAD="${LD_PRELOAD}:${JSIG}" ./ProjectZomboid64 "$@"
-
